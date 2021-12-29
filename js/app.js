bbeee5fc63751f21d992023ba595f9f8d9b138f4
jafacorp <56782606+abodojustin@users.noreply.github.com>
2021-12-29 13:19:37 +0100

    les filtres en Vue Js

--- js/app.js
+++ js/app.js
@@ -1,37 +1,12 @@
-// première façon d'ecrire les directives
-/* Vue.directive("salut", {
-  bind: function (el, binding, vnode) {
-    el.value = binding.value;
-    console.log("bind");
-  },
-  update: function (el, binding, oldvnode) {
-    console.log("update");
-  },
-}); */
-
-// meilleur façon d'écrire les directives
-let salut = function (el, binding) {
-  el.value = binding.value;
-  console.log("bind");
-};
-
-let vm = new Vue({
+Vue.filter("capitalize", function (value) {
+  return value.toUpperCase();
+});
+Vue.filter("reverse", function (value) {
+  return value.split(" ").reverse().join(" ");
+});
+new Vue({
   el: "#app",
-  directives: {
-    salut,
-  },
   data: {
-    firstname: "Francis",
-    lastname: "Abodo",
-    fullname: "",
-    message: "hello",
-  },
-  methods: {
-    demo: function () {
-      console.log("Demo");
-    },
-    demo2: function () {
-      console.log("Demo 2");
-    },
+    message: "Salut les gens",
   },
 });
