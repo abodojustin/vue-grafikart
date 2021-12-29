16edf045b6de25ab3a4fdb3ae1d320b3309f5d12
jafacorp <56782606+abodojustin@users.noreply.github.com>
2021-12-29 12:25:19 +0100

    les directives sur Vue Js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,37 @@
+// première façon d'ecrire les directives
+/* Vue.directive("salut", {
+  bind: function (el, binding, vnode) {
+    el.value = binding.value;
+    console.log("bind");
+  },
+  update: function (el, binding, oldvnode) {
+    console.log("update");
+  },
+}); */
+
+// meilleur façon d'écrire les directives
+let salut = function (el, binding) {
+  el.value = binding.value;
+  console.log("bind");
+};
+
 let vm = new Vue({
   el: "#app",
+  directives: {
+    salut,
+  },
   data: {
     firstname: "Francis",
     lastname: "Abodo",
     fullname: "",
+    message: "hello",
   },
-  watch: {
-    fullname: function (value) {
-      console.log("watch", value);
+  methods: {
+    demo: function () {
+      console.log("Demo");
+    },
+    demo2: function () {
+      console.log("Demo 2");
     },
   },
 });
