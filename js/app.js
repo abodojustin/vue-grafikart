674edcbea056e5931d10008118dd55bc25f2128a
jafacorp <56782606+abodojustin@users.noreply.github.com>
2022-01-01 16:38:45 +0100

    components

--- js/app.js
+++ js/app.js
@@ -1,12 +1,38 @@
-Vue.filter("capitalize", function (value) {
-  return value.toUpperCase();
-});
-Vue.filter("reverse", function (value) {
-  return value.split(" ").reverse().join(" ");
-});
-new Vue({
+let message = {
+  props: {
+    type: { type: String, default: "success" },
+    message: String,
+  },
+  template: `<div class="ui message" :class="type"> {{ message }}</div>`,
+};
+
+let counter = {
+  data: function () {
+    return {
+      count: 0,
+    };
+  },
+  props: {
+    start: { type: Number, default: 0 },
+  },
+  methods: {
+    increment: function () {
+      this.count++;
+    },
+  },
+  template: `<div>
+        
+        <button @click="increment">{{ count }}</button>
+    </div>`,
+  mounted: function () {
+    this.count = this.start;
+  },
+};
+
+let vm = new Vue({
   el: "#app",
   data: {
     message: "Salut les gens",
   },
+  components: { message, counter },
 });
