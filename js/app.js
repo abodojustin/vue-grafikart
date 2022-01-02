1a88ef81e1d49f2bf3f2f36b036e6474e5879f2b
jafacorp <56782606+abodojustin@users.noreply.github.com>
2022-01-02 08:37:06 +0100

    Les Composants

--- js/app.js
+++ js/app.js
@@ -2,8 +2,18 @@ let message = {
   props: {
     type: { type: String, default: "success" },
     message: String,
+    header: String,
   },
-  template: `<div class="ui message" :class="type"> {{ message }}</div>`,
+  template: `<div class="ui message" :class="type">
+        <i class="close icon" @click.prevent="close"></i>
+        <div class="header">{{ header }}</div>
+        {{ message }}
+    </div>`,
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
 };
 
 let counter = {
@@ -20,19 +30,67 @@ let counter = {
       this.count++;
     },
   },
-  template: `<div>
-        
-        <button @click="increment">{{ count }}</button>
-    </div>`,
+  template: `<button @click="increment">{{ count }}</button>`,
   mounted: function () {
     this.count = this.start;
   },
 };
 
+let formUser = {
+  props: {
+    value: Object,
+  },
+  data() {
+    return {
+      user: JSON.parse(JSON.stringify(this.value)),
+    };
+  },
+  methods: {
+    save() {
+      this.$emit("input", this.user);
+    },
+  },
+  template: `
+        <form class="ui form" @submit.prevent="save">
+        <p>
+                <slot name="header"></slot>
+            </p>
+            <div class="field">
+                <label for="">Prénom</label>
+                <input type="text" v-model="user.firstname" />
+            </div>
+            <div class="field">
+                <label for="">Nom</label>
+                <input type="text" v-model="user.lastname" />
+            </div>
+            <button class="ui button" type="submit">Envoyer</button>
+            <p>
+                <slot name="footer"></slot>
+            </p>
+        </form>
+    `,
+  mounted: function () {
+    console.log(this);
+  },
+};
+
 let vm = new Vue({
   el: "#app",
+  components: { message, counter, formUser },
   data: {
     message: "Salut les gens",
+    alert: false,
+    user: {
+      firstname: "Jean",
+      lastname: "DeLaTour",
+    },
   },
-  components: { message, counter },
+  methods: {
+    showAlert() {
+      this.alert = true;
+    },
+    hideAlert() {
+      this.alert = false;
+    },
+  },
 });
