da713e72777f7ea48d58ad97da4b1c88ba6e94d7
jafacorp <56782606+abodojustin@users.noreply.github.com>
2022-01-02 10:12:47 +0100

    vue-cli

---
+++ vuejs/src/Counter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="counter">
+    {{ count }}
+    <div class="counter__button" @click="increment">+</div>
+  </div>
+</template>
+
+<script>
+import store from "./store/NotificationsStore";
+export default {
+  data() {
+    return {
+      state: store.state,
+    };
+  },
+  computed: {
+    count() {
+      return this.state.count;
+    },
+  },
+  methods: {
+    increment() {
+      store.increment();
+    },
+  },
+};
+</script>
+
+<style>
+.counter {
+  font-weight: bold;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  background-color: #dfdfdf;
+}
+.counter__button {
+  border: 1px solid #000;
+  padding: 2px 10px;
+}
+</style>
